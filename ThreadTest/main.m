98f999b86cb776b6aa44cb8ffe6f8666f345f7d3
mojopedia <mojopedia@mojopedias-MacBook-Pro.local>
2014-09-19 15:27:26 +0900

    Initial Commit

---
+++ ThreadTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreadTest
+//
+//  Created by mojopedia on 9/19/14.
+//  Copyright (c) 2014 WITCHCRAFTWORKS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
